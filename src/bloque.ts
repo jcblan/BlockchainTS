4990a89220d4bfa80149ae65e1bb7cb4eaf05310
JcBlan <blancojulic@gmail.com>
2021-04-12 19:17:36 -0300

    Refactor bloque, eliminado genesis

--- src/bloque.ts
+++ src/bloque.ts
@@ -4,20 +4,24 @@ export class Bloque{
 
     _hash: string;
     _hash_anterior: string;
-    _transaccion: string;
+    _archivo: string;
+    _motivo: string;
+    _fecha: Date
     
-    constructor(transaccion: string, hash_anterior?: string){
-        this.hash = String(SHA256(transaccion));
-        this.transaccion = transaccion;
+    constructor(fecha: Date, hash_anterior: string, motivo: string, archivo:string){
+        this.fecha  = fecha;
+        this.archivo = archivo;
+        this.motivo = motivo;
         this.hash_anterior = hash_anterior;
+        this.hash = String(SHA256(String(this.fecha).concat(this.hash_anterior, this.motivo, this.archivo) ));
     }
 
-    get transaccion(){
-        return this._transaccion;
+    get archivo(){
+        return this._archivo;
     }
 
-    set transaccion(valor: string){
-        this._transaccion = valor;
+    set archivo(valor: string){
+        this._archivo = valor;
     }
 
     get hash(){
@@ -36,7 +40,20 @@ export class Bloque{
         this._hash_anterior = valor;
     }
 
-    static genesis(){
-        return new Bloque("genesis");
+    get fecha(){
+        return this._fecha;
     }
+
+    set fecha(valor: Date){
+        this._fecha = valor;
+    }
+
+    get motivo(){
+        return this._motivo;
+    }
+
+    set motivo(valor: string){
+        this._motivo = valor
+    }
+
 }
--- tests/block.test.ts
+++ tests/block.test.ts
@@ -3,17 +3,15 @@ import { Bloque } from '../src/bloque';
 import { SHA256 } from 'crypto-js';
 
 describe('Block.constructor', () => {
-    it('Datos de transacción debera crear bloque con transacción', () => {  
-        const blck = new Bloque("Test Transacción");
-        expect(blck.transaccion).to.equal("Test Transacción");
-    });
-    it('Datos de transacción debera crear bloque con hash de transacción', () => {  
-        const blck = new Bloque("Test Transacción");
-        const hash_trans = String(SHA256("Test Transacción"));
-        expect(blck.hash).to.equal(hash_trans);
-    });
-    it('Hash anterior de bloque 1 debera ser bloque genesis', () => {  
-        const blck = new Bloque("Test Transacción", Bloque.genesis().hash);
-        expect(blck.hash_anterior).to.equal(Bloque.genesis().hash);
+    it('fecha, hash anterior, motivo y archivo debera crear bloque hasheando fecha, hash anterior, motivo y archivo ', () => {  
+        const hash_anterior = String(SHA256("anterior"))
+        const archivo = "archivo"
+        const motivo = "motivo"
+        const fecha = new Date()
+
+        const blqe = new Bloque(fecha, hash_anterior, motivo, archivo);
+
+        const hash_test = String(SHA256(String(fecha).concat(hash_anterior, motivo, archivo)));
+        expect(blqe.hash).to.equal(hash_test);
     });
 });

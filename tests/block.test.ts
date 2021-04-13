59789035549dcd835702eb5b10e2b3f233226506
JcBlan <blancojulic@gmail.com>
2021-04-13 18:33:02 -0300

    Añadido test generar bloque con datos

--- tests/block.test.ts
+++ tests/block.test.ts
@@ -14,6 +14,21 @@ describe('Block.constructor', () => {
 
         expect(blqe.hash).to.equal("378ac150717b50f41633c5701d12ce59d3cdcb41a0a926f0588d89f52ee19f8f");
     });
+    it('fecha, hash anterior, motivo y archivo debera crear bloque hasheando fecha, hash anterior, motivo y archivo ', () => {  
+        const hash_anterior = String(SHA256("anterior"))
+        const archivo = "archivo"
+        const motivo = "motivo"
+        const fecha = new Date(2021, 0O3, 21, 19, 30 ,0O0)
+        const email = "email@email.com"
+
+        const blqe = new Bloque(fecha, hash_anterior, motivo, archivo, email);
+        
+        expect(blqe.archivo).to.equal("archivo");
+        expect(blqe.motivo).to.equal("motivo");
+        expect(blqe.fecha.toString()).to.equal("Wed Apr 21 2021 19:30:00 GMT-0300 (hora estándar de Argentina)");
+        expect(blqe.email).to.equal("email@email.com");
+
+    });
 
     it('Dos bloque con los mismos datos deberan tener el mismo hash ', () => {  
         const hash_anterior = String(SHA256("anterior"))

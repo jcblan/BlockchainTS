6930948b60b36829bb1c4239225f2f2cfd9341af
JcBlan <blancojulic@gmail.com>
2021-04-21 17:42:17 -0300

    Modificado unit test para mejorar testeo de singleton

--- tests/blockchain.test.ts
+++ tests/blockchain.test.ts
@@ -1,6 +1,5 @@
 import { expect } from 'chai';
 import { Blockchain } from '../src/blockchain';
-import { Bloque } from '../src/bloque';
 
 
 describe('Blockchain.constructor', () => {
@@ -15,6 +14,8 @@ describe('Blockchain.constructor', () => {
     it('Instancia blockchain debera crear bloque genesis en blockchain', () => {  
        
         const blckchain = Blockchain.getInstancia();
+        blckchain.inicializar();
+
         expect(blckchain.getBloque(0).archivo).to.equal("genesis");
         expect(blckchain.getBloque(0).hash_anterior).to.equal("undefined");
         expect(blckchain.getBloque(0).motivo).to.equal("genesis");
@@ -22,17 +23,19 @@ describe('Blockchain.constructor', () => {
     it('Generar un segundo bloque debera añadir el bloque despues del genesis ', () => {  
        
         const blckchain = Blockchain.getInstancia();
+        blckchain.inicializar();
 
-        blckchain.generarBloque("motivo 2do bloque", "archivo 2do bloque", "2doBloque@email.com");
+        blckchain.generarBloque("motivo 1er bloque", "archivo 1er bloque", "1erBloque@email.com");
 
         expect(blckchain.getBloque(1).hash_anterior).to.equal(blckchain.getBloque(0).hash);
     });
     it('Generar dos bloques debera añadir el primero despues del genesis y el segundo despues del primero', () => {  
        
         const blckchain = Blockchain.getInstancia();
+        blckchain.inicializar();
 
-        blckchain.generarBloque("motivo 3er bloque", "archivo 3er bloque", "3erBloque@email.com");
-        blckchain.generarBloque("motivo 4to bloque", "archivo 4to bloque", "4toBloque@email.com");
+        blckchain.generarBloque("motivo 1er bloque", "archivo 1er bloque", "1erBloque@email.com");
+        blckchain.generarBloque("motivo 2do bloque", "archivo 2do bloque", "2doBloque@email.com");
 
         expect(blckchain.getBloque(1).hash_anterior).to.equal(blckchain.getBloque(0).hash);
         expect(blckchain.getBloque(2).hash_anterior).to.equal(blckchain.getBloque(1).hash);
@@ -40,6 +43,10 @@ describe('Blockchain.constructor', () => {
     it('Debera devolver array con indices si se verifica que falla la integridad del bloque en ese indice', () => {  
        
         const blckchain = Blockchain.getInstancia();
+        blckchain.inicializar();
+        
+        blckchain.generarBloque("motivo 1er bloque", "archivo 1er bloque", "1erBloque@email.com");
+        blckchain.generarBloque("motivo 2do bloque", "archivo 2do bloque", "2doBloque@email.com");
 
         expect(blckchain.verificarBlockchain()).to.equal(true);
     });
@@ -47,6 +54,9 @@ describe('Blockchain.constructor', () => {
     it('Debera devolver bloque que concida con hash de busqueda', () => {  
        
         const blckchain = Blockchain.getInstancia();
+        blckchain.inicializar();
+
+        blckchain.generarBloque("motivo 1er bloque", "archivo 1er bloque", "1erBloque@email.com");
 
         const hash_test = blckchain.getBloque(1).hash
         expect(blckchain.buscarPorHash(hash_test).hash).to.equal(blckchain.getBloque(1).hash);

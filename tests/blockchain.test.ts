4961bd2c967ee1fe6966b8a1d8f5576b9d528d3c
JcBlan <blancojulic@gmail.com>
2021-04-21 21:21:48 -0300

    Añadido getUltimoBloque, unit test getUltimoBloque y unit test 100 bloques

--- tests/blockchain.test.ts
+++ tests/blockchain.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { Blockchain } from '../src/blockchain';
+import { HashGeneratorStrategy } from '../src/hashGeneratorStrategy';
 
 
 describe('Blockchain.constructor', () => {
@@ -40,6 +41,7 @@ describe('Blockchain.constructor', () => {
         expect(blckchain.getBloque(1).hash_anterior).to.equal(blckchain.getBloque(0).hash);
         expect(blckchain.getBloque(2).hash_anterior).to.equal(blckchain.getBloque(1).hash);
     });
+    
     it('Debera devolver true si se verifica la integridad de la blockchain', () => {  
        
         const blckchain = Blockchain.getInstancia();
@@ -64,7 +66,6 @@ describe('Blockchain.constructor', () => {
     });
 
 
-
     it('Debera devolver bloque que concida con hash de busqueda', () => {  
        
         const blckchain = Blockchain.getInstancia();
@@ -86,4 +87,31 @@ describe('Blockchain.constructor', () => {
         
         expect(blckchain.buscarPorHash("b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79")).to.equal(null);
     });
+
+    it('getUltimoBloque debera devolver el ultimo bloque de la cadena', () => {  
+       
+        const blckchain = Blockchain.getInstancia();
+        blckchain.inicializar();
+
+        blckchain.generarBloque("motivo 1er bloque", "archivo 1er bloque", "1erBloque@email.com");
+        blckchain.generarBloque("motivo 2do bloque", "archivo 2do bloque", "2doBloque@email.com");
+    
+        expect(blckchain.getUltimoBloque()).to.equal(blckchain.getBloque(2));
+    });
+
+    it('El hash_anterior del bloque 50 debera ser el hash del bloque 49. GenerarHash del Bloque 50 debera coincidir con el hash del bloque 50 ', () => {  
+       
+        const blckchain = Blockchain.getInstancia();
+        blckchain.inicializar();
+
+        for(let i = 1; i < 100; i++){
+            blckchain.generarBloque("motivo".concat(String(i)),"archivo".concat(String(i)),"email".concat(String(i)));
+
+        }
+        const strategy = HashGeneratorStrategy.getStrategy(new Date().getDate());
+        const hash_test = strategy.generarHash(blckchain.getBloque(50).toString());
+        expect(blckchain.getBloque(50).hash_anterior).to.equal(blckchain.getBloque(49).hash);
+        expect(blckchain.getBloque(50).hash).to.equal(hash_test);
+    });
+
 });
